7ba818174fe5ff4317e25d8ce23789b03d9ebdaf
PandaPandaPanda <34975856+PandaPandaPanda@users.noreply.github.com>
2022-07-15 19:31:21 -0400

    Setup Observer Pattern

--- Cell.h
+++ Cell.h
@@ -1,9 +1,14 @@
 #ifndef __Cell_h__
 #define __Cell_h__
 #include "ChessPiece.h"
+#include "TextDisplay.h"
 
 class Cell {
   ChessPiece *p;
+  TextDisplay *td;
 
+  public:
+  void attach(); // attach observer
+  void notifyGraphicOvserver();
 };
 #endif
--- TextDisplay.h
+++ TextDisplay.h
@@ -1,7 +1,15 @@
 #ifndef __TextDisplay_h__
 #define __TextDisplay_h__
+#include "vector"
 
 class TextDisplay {
+  std::vector<std::vector<char>> theDisplay;
+public:
+  TextDisplay(int n);
+  ~TextDisplay();
 
+  void notify(Cell &c) override;
+
+  friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
 };
 #endif
