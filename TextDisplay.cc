52fb2dc669b9dc7c7852764baeb7d53d043c976b
Kevin Xu <emailofkevinxu@gmail.com>
2022-07-23 22:31:43 -0400

    Fixed TextDispaly Two Side Same Piece Problem

--- TextDisplay.cc
+++ TextDisplay.cc
@@ -6,7 +6,19 @@
 using namespace std;
 
 TextDisplay::TextDisplay() {
-      theDisplay = vector<vector<char>>(8, vector<char>(8, ' '));
+      theDisplay = vector<vector<string>>(8, vector<string>(8, " "));
+      emojiMap['P'][Color::Black] = "♟";
+      emojiMap['R'][Color::Black] = "♜";
+      emojiMap['N'][Color::Black] = "♞";
+      emojiMap['B'][Color::Black] = "♝";
+      emojiMap['Q'][Color::Black] = "♛";
+      emojiMap['K'][Color::Black] = "♚";
+      emojiMap['P'][Color::White] = "♙";
+      emojiMap['R'][Color::White] = "♖";
+      emojiMap['N'][Color::White] = "♘";
+      emojiMap['B'][Color::White] = "♗";
+      emojiMap['Q'][Color::White] = "♕";
+      emojiMap['K'][Color::White] = "♔";
 }
 
 TextDisplay::~TextDisplay() {
@@ -14,18 +26,21 @@ TextDisplay::~TextDisplay() {
 }
 
 void TextDisplay::notify(Cell &c) {
-  // todo
   int row = c.getRow();
   int col = c.getCol();
   if (c.getChessPiece()) {
-    theDisplay[row][col] = c.getChessPiece()->getType();
+    theDisplay[row][col] = emojiMap[c.getChessPiece()->getType()][c.getChessPiece()->getColor()] + " ";
   } else {
-    theDisplay[row][col] = ' ';
+    theDisplay[row][col] = "  ";
   }
 }
 
 ostream &operator<<(ostream &out, const TextDisplay &td) {
+  int rowCount = 8;
   for (auto& row : td.theDisplay) {
+    out << rowCount << " ";
+    rowCount--;
+
     out << "|";
     for (auto& cell : row) {
       out << cell;
@@ -33,5 +48,11 @@ ostream &operator<<(ostream &out, const TextDisplay &td) {
     }
     out << '\n';
   }
+
+  out << "   ";
+  for (int i = 0; i < 8; i++) {
+    out << (char)(i + 'a') << "  ";
+  }
+  out << endl;
   return out;
 }
--- TextDisplay.h
+++ TextDisplay.h
@@ -1,12 +1,18 @@
 #ifndef __TextDisplay_h__
 #define __TextDisplay_h__
+
+#include "Color.h"
+
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 class Cell;
 
 class TextDisplay {
-  std::vector<std::vector<char>> theDisplay;
+  std::vector<std::vector<std::string>> theDisplay;
+  std::unordered_map<char, std::unordered_map<Color, std::string>> emojiMap;
+
   //K = King
   //Q = Queen
   //R = Rook
